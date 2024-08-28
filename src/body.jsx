4c97a159decd6e89ee3dd7d64cf325057afe8fcf
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-08-28 11:27:50 +0100

    go

--- src/body.jsx
+++ src/body.jsx
@@ -1,5 +1,5 @@
 import myImage from './assets/cardimage.png';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Card() {
     return(
@@ -16,46 +16,45 @@ function Card() {
 
 function Body() {
 
-      useEffect(() => {
-        async function fetchPokemon() {
-          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/bulbasaur`);
-          const data = await response.json();
-          
-          console.log(data)
-        }
-    
-        fetchPokemon();
-      }, []);
+    const [address, setAddress] = useState("lagos")
+
+    useEffect(() => {
+
+        fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${address}?key=RG6TK6E2VCGQ377MRP7SWA6HJ`)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => console.log(error))
+
+    }, [address]);
+
+
+    const handleSubmit = () => {
+        let addressInput = document.querySelector("#address");
+        setAddress(addressInput.value);
+    }
 
     return(
         <>
         <div className={"score-line"}>
-
             <div className={"game-name"}>
-                <h2>Memory Game</h2>
+                <h2>{ address }</h2>
                 <p>Get points by clicking on an image but don't click on any more than once!</p>
             </div>
-
             <div className={"game-score"}>
                 <p>Score: 0</p>
                 <p>Best Score: 8</p>
             </div>
-
         </div>
 
         <div className={"container"}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card/>
+        </div>
+
+        <div className='input'>
+            <input type="text" id='address'/>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
         </>
     );
